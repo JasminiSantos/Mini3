cbcab6c99e510d5782e112eb783d2c215ab409ef
jasminisantos <jasminirebeca10@gmail.com>
2023-10-29 20:14:45 -0300

    Initial Commit

---
+++ Mini3/Mini3App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini3App.swift
+//  Mini3
+//
+//  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
