a9c28640b3f5b0ec67e4422ce119bdc3bca6c2a1
jasminisantos <jasminirebeca10@gmail.com>
2023-10-30 01:05:09 -0300

    drawing tool enum

--- Mini3/Enum/DrawingTool.swift
+++ Mini3/Enum/DrawingTool.swift
@@ -1,8 +1,13 @@
 //
-//  DrawingTools.swift
+//  DrawingTool.swift
 //  Mini3
 //
 //  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+enum DrawingTool {
+    case pencil(Color)
+    case eraser
+}
