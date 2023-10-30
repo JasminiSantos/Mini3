8ead674c39b45052b30f06e9bcd81771705a9a9c
jasminisantos <jasminirebeca10@gmail.com>
2023-10-30 01:01:36 -0300

    creating custom color picker

--- Mini3/Component/CustomColorPicker.swift
+++ Mini3/Component/CustomColorPicker.swift
@@ -5,4 +5,37 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomColorPicker: View {
+    @Binding var selectedColor: Color
+    
+    private let colors: [Color] = [.black, .red, .green, .blue, .orange, .pink, .purple, .yellow, .gray]
+    
+    var body: some View {
+        HStack {
+            ForEach(colors, id: \.self) { color in
+                Circle()
+                    .fill(color)
+                    .frame(width: 30, height: 30)
+                    .overlay(
+                        Circle()
+                            .stroke(selectedColor == color ? Color.black : Color.clear, lineWidth: 2)
+                    )
+                    .onTapGesture {
+                        selectedColor = color
+                    }
+            }
+        }
+        .padding(10)
+        .background(Color.white)
+        .cornerRadius(15)
+        .shadow(radius: 5)
+    }
+}
+
+
+struct ColorItem: Identifiable {
+    let id: UUID = UUID()
+    let color: Color
+}
