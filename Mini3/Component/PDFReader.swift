45bc8a30281acb5162389a7142aa4bb0d0cd5243
jasminisantos <jasminirebeca10@gmail.com>
2023-10-30 01:03:08 -0300

    creating pdf reader component

--- Mini3/Component/PDFReader.swift
+++ Mini3/Component/PDFReader.swift
@@ -5,4 +5,32 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
 //
 
-import Foundation
+import SwiftUI
+import PDFKit
+
+
+struct PDFReader: UIViewRepresentable {
+    var url: URL
+    
+    func makeUIView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.autoScales = true
+        loadPDF(pdfView)
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: PDFView, context: Context) {}
+    
+    func loadPDF(_ pdfView: PDFView) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data, let document = PDFDocument(data: data) {
+                DispatchQueue.main.async {
+                    pdfView.document = document
+                }
+            } else {
+                print("Failed to load PDF: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+        task.resume()
+    }
+}
