c151654e21cce14220f1fecb8a2ceca098472279
jasminisantos <jasminirebeca10@gmail.com>
2023-10-30 01:02:21 -0300

    creaing custom drawing tool bar

--- Mini3/Component/DrawingToolBar.swift
+++ Mini3/Component/DrawingToolBar.swift
@@ -5,4 +5,44 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
 //
 
-import Foundation
+import SwiftUI
+import PencilKit
+
+struct DrawingToolbar: View {
+    @Binding var selectedTool: DrawingTool
+    @Binding var selectedColor: Color
+    @Binding var showColorPicker: Bool
+    @Binding var clearCanvas: Bool
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                selectedTool = .pencil(selectedColor)
+            }) {
+                Image(systemName: "pencil")
+            }
+            
+            Button(action: {
+                selectedTool = .eraser
+            }) {
+                Image(systemName: "eraser")
+            }
+            
+            Button(action: {
+                showColorPicker.toggle()
+            }) {
+                Image(systemName: "paintpalette")
+            }
+            
+            Button(action: {
+                clearCanvas = true
+            }) {
+                Image(systemName: "trash")
+            }
+        }
+        .padding()
+        .background(Color.white.opacity(0.7))
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+}
