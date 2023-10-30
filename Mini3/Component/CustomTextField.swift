cab380639b4c85ceaa1c28b98c814d54fdf70939
jasminisantos <jasminirebeca10@gmail.com>
2023-10-30 01:01:59 -0300

    creating custom text field

--- Mini3/Component/CustomTextField.swift
+++ Mini3/Component/CustomTextField.swift
@@ -5,4 +5,18 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomTextField: View {
+    @Binding var text: String
+    var placeholder: String
+    var onCommit: () -> Void
+    
+    var body: some View {
+        TextField(placeholder, text: $text, onCommit: onCommit)
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(8)
+            .padding(.horizontal)
+    }
+}
