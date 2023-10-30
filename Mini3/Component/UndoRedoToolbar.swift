e4f44a8ef28252cc6bf7c78dad63fa1bf7426804
jasminisantos <jasminirebeca10@gmail.com>
2023-10-30 01:03:44 -0300

    creating undo-redo component

--- Mini3/Component/UndoRedoToolbar.swift
+++ Mini3/Component/UndoRedoToolbar.swift
@@ -5,4 +5,36 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 29/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct UndoRedoToolbar: View {
+    var undoAction: () -> Void
+    var redoAction: () -> Void
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                print("Undo Pressed")
+                undoAction()
+            }
+            ) {
+                Image(systemName: "arrow.uturn.backward.circle.fill")
+                    .font(.system(size: 24))
+            }
+            .buttonStyle(.plain)
+            
+            Button(action: {
+                print("Redo Pressed")
+                redoAction()
+            }) {
+                Image(systemName: "arrow.uturn.forward.circle.fill")
+                    .font(.system(size: 24))
+            }
+            .buttonStyle(.plain)
+        }
+        .foregroundColor(.primary)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray.opacity(0.2)))
+    }
+}
+
