07884e7b04ab93008c7d4a1a57fbfed40ebc739c
naocolenicole <nicolecardoso09@gmail.com>
2023-11-03 13:48:30 -0300

    PetProfileView e TutorProfileView
    card das telas de criar perfil do pet e editar dados do pet/tutor

---
+++ Mini3/View/PetProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  PetProfileView.swift
+//  Mini3
+//
+//  Created by Nicole Cardoso Machado on 02/11/23.
+//
+
+import SwiftUI
+
+struct PetProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetProfileView()
+}
