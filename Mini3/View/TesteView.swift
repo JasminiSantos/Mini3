8152b33def68e359750aa75fda71913f2b455ac9
jasminisantos <jasminirebeca10@gmail.com>
2023-11-06 19:41:30 -0300

    adding view for testing of components

--- Mini3/View/TesteView.swift
+++ Mini3/View/TesteView.swift
@@ -5,4 +5,89 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 03/11/23.
 //
 
-import Foundation
+import SwiftUI
+import PDFKit
+
+
+@MainActor
+struct TesteView: View {
+    @State private var pdfURL: URL?
+    var body: some View {
+        VStack{
+            
+            PDFDataView()
+            
+            Button("Generate PDF") {
+                pdfURL = generatePDF()
+            }
+            .padding()
+            if let pdfURL = pdfURL {
+                PDFKitView(url: pdfURL)
+                
+                ShareLink("Export PDF", item: pdfURL)
+            }
+        }
+    }
+    
+    
+    // PDF Viewer
+    struct PDFKitView: UIViewRepresentable {
+        
+        let url: URL
+        
+        func makeUIView(context: Context) -> PDFView {
+            let pdfView = PDFView()
+            pdfView.document = PDFDocument(url: self.url)
+            pdfView.autoScales = true
+            return pdfView
+        }
+        
+        func updateUIView(_ pdfView: PDFView, context: Context) {
+            // Update pdf if needed
+        }
+    }
+    
+    
+    // generate pdf from given view
+    func generatePDF() -> URL {
+        //  Select UI View to render as pdf
+        let renderer = ImageRenderer(content:PDFDataView())
+        
+        let url = URL.documentsDirectory.appending(path: "generatedPDF.pdf")
+       
+        renderer.render { size, context in
+            var pdfDimension = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            
+            guard let pdf = CGContext(url as CFURL, mediaBox: &pdfDimension, nil) else {
+                return
+            }
+            pdf.beginPDFPage(nil)
+            context(pdf)
+            pdf.endPDFPage()
+            pdf.closePDF()
+        }
+        
+        return url
+    }
+}
+
+//
+struct PDFDataView: View {
+    var body: some View {
+        VStack{
+            Text("This view will be added into PDF")
+                .font(.title)
+            Divider()
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Divider()
+            Image("apple")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth:  200, maxHeight: 200)
+
+        }.padding()
+        
+    }
+}
