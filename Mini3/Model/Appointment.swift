8f4e8a8dba91a87049d322336202b3589a5c38ab
jasminisantos <jasminirebeca10@gmail.com>
2023-11-06 19:44:06 -0300

    adding models for pet, appointment

--- Mini3/Model/Appointment.swift
+++ Mini3/Model/Appointment.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Appointment: Codable {
+    var date: String
+    var time: String
+    var weight: Double
+    var bodyCondition: String
+    var reasonForVisit: String
+    var observations: String
+    var treatment: String
+}
+
+class AppointmentModel: ObservableObject {
+    @Published var date: String
+    @Published var time: String
+    @Published var weight: Double
+    @Published var bodyCondition: String
+    @Published var reasonForVisit: String
+    @Published var observations: String
+    @Published var treatment: String
+    
+    init(date: String, time: String, weight: Double, bodyCondition: String, reasonForVisit: String, observations: String, treatment: String) {
+        self.date = date
+        self.time = time
+        self.weight = weight
+        self.bodyCondition = bodyCondition
+        self.reasonForVisit = reasonForVisit
+        self.observations = observations
+        self.treatment = treatment
+    }
+}
--- Mini3/Model/Pet.swift
+++ Mini3/Model/Pet.swift
@@ -5,4 +5,39 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 02/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct Pet: Codable {
+    var name: String
+    var species: String
+    var breed: String
+    var age: String
+    var gender: String
+    var furColor: String
+    var tutor: String
+    var cpf: String
+    var address: String
+}
+class PetModel: ObservableObject {
+    @Published var name: String
+    @Published var species: String
+    @Published var breed: String
+    @Published var age: String
+    @Published var gender: String
+    @Published var furColor: String
+    @Published var tutor: String
+    @Published var cpf: String
+    @Published var address: String
+    
+    init(name: String, species: String, breed: String, age: String, gender: String, furColor: String, tutor: String, cpf: String, address: String) {
+        self.name = name
+        self.species = species
+        self.breed = breed
+        self.age = age
+        self.gender = gender
+        self.furColor = furColor
+        self.tutor = tutor
+        self.cpf = cpf
+        self.address = address
+    }
+}
