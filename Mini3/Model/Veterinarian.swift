869df1592818478adbfb8bf4f9772d48f49d0071
jasminisantos <jasminirebeca10@gmail.com>
2023-11-06 19:33:44 -0300

    creating veterinarian model

--- Mini3/Model/Veterinarian.swift
+++ Mini3/Model/Veterinarian.swift
@@ -5,4 +5,25 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 02/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct Veterinarian: Codable {
+    var name: String
+    var email: String
+    var crmv: String
+    var phoneNumber: String
+}
+
+class VeterinarianModel: ObservableObject {
+    @Published var name: String
+    @Published var email: String
+    @Published var crmv: String
+    @Published var phoneNumber: String
+    
+    init(name: String, email: String, crmv: String, phoneNumber: String) {
+        self.name = name
+        self.email = email
+        self.crmv = crmv
+        self.phoneNumber = phoneNumber
+    }
+}
--- Mini3/View/PDFKitView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PDFKitView.swift
-//  Mini3
-//
-//  Created by Jasmini Rebecca Gomes dos Santos on 03/11/23.
-//
-
-import Foundation
