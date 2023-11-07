8fada24381400b5165072be6dcc97a79040ec31a
naocolenicole <nicolecardoso09@gmail.com>
2023-11-07 14:45:49 -0300

    Pet profile view
    subindo a tela de criação do perfil do pet

---
+++ Mini3/View/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Mini3
+//
+//  Created by Nicole Cardoso Machado on 06/11/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
