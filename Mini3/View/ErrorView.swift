14a3310655f7bbfbb238760323ecf5f2910487cf
naocolenicole <nicolecardoso09@gmail.com>
2023-11-08 16:16:06 -0300

    Onboarding, error e final
    telas

---
+++ Mini3/View/ErrorView.swift
@@ -0,0 +1,84 @@
+//
+//  ErrorView.swift
+//  Mini3
+//
+//  Created by Nicole Cardoso Machado on 08/11/23.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    struct Constants {
+        static let AzulEscuro: Color = Color(red: 0, green: 0.21, blue:0.35)
+        static let AzulClaro: Color = Color(red: 0.42, green: 0.6, blue: 0.77)
+        static let Branco: Color = Color(red: 0.94, green: 0.95, blue: 0.95)
+        static let Menta: Color = Color(red: 0.45, green: 0.78, blue: 0.62)
+        static let VermelhoAlaranjado: Color = Color(red: 0.91, green: 0.44, blue: 0.32)
+        }
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Constants.VermelhoAlaranjado)
+            
+            VStack (spacing: 30){
+                Image(systemName: "x.circle")
+                    .font(.system(size: 200, weight: .semibold))
+                    .foregroundColor(Constants.AzulEscuro)
+                
+                Text("Ah não!")
+                    .font(.system(size: 70, weight: .bold))
+                    .foregroundColor(Constants.AzulEscuro)
+                
+                Text("Identificamos um erro ao finalizar o seu prontuário, revise-o ou tente novamente mais tarde :(")
+                    .font(.system(size: 32, weight: .semibold))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30)
+                
+                VStack (spacing: 30){
+                    Button(action: {}){
+                        HStack(spacing: 20){
+                            Image(systemName: "paperplane")
+                                .font(.system(size: 32, weight: .medium))
+                                .foregroundColor(Constants.AzulEscuro)
+                            
+                            Text("Voltar ao prontuário")
+                                .font(.system(size: 32, weight: .bold))
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Constants.AzulEscuro)
+                        }
+                    }
+                    .foregroundColor(.clear)
+                    .frame(width: 430, height: 60)
+                    .background(.white)
+                    .cornerRadius(18)
+                    
+                    
+                    Button(action: {}){
+                        HStack(spacing: 20){
+                            Image(systemName: "house")
+                                .font(.system(size: 32, weight: .medium))
+                                .foregroundColor(Constants.AzulEscuro)
+                            
+                            Text("Ir para o início")
+                                .font(.system(size: 32, weight: .bold))
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Constants.AzulEscuro)
+                        }
+                    }
+                    .foregroundColor(.clear)
+                    .frame(width: 430, height: 60)
+                    .background(.white)
+                    .cornerRadius(18)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ErrorView()
+}
--- Mini3/View/OnboardingView.swift
+++ Mini3/View/OnboardingView.swift
@@ -24,13 +24,112 @@ struct OnboardingView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Constants.AzulClaro)
             
-            Text("Bem-vindo ao")
-                .font(.system(size: 50, weight: .semibold))
-                .foregroundColor(.white)
+            VStack (spacing: 60){
+                VStack (spacing: 10){
+                    Text("Bem-vindo ao")
+                        .font(.system(size: 70, weight: .semibold))
+                        .foregroundColor(.white)
+                    
+                    Image("VetPad")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 500, maxHeight: 70)
+                }
+                
+                VStack (spacing: 20){
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(maxWidth: 730, maxHeight: 200)
+                            .background(.white)
+                            .cornerRadius(30)
+                        
+                        HStack(alignment: .center, spacing: 20) {
+                            Text("1")
+                                .font(.system(size: 130, weight: .heavy))
+                                .foregroundColor(Constants.AzulEscuro)
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100, alignment: .center)
+                            
+                            Text("Preencha seus dados como médico veterinário")
+                                .font(.system(size: 32, weight: .semibold))
+                                .foregroundColor(Constants.AzulEscuro)
+                        }
+                        .padding(.horizontal, 80)
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(maxWidth: 730, maxHeight: 200)
+                            .background(.white)
+                            .cornerRadius(30)
+                        
+                        HStack(alignment: .center, spacing: 20) {
+                            Text("2")
+                                .font(.system(size: 130, weight: .heavy))
+                                .foregroundColor(Constants.AzulEscuro)
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100, alignment: .center)
+                            
+                            Text("Preencha o cadastro básico do seu paciente e seu tutor")
+                                .font(.system(size: 32, weight: .semibold))
+                                .foregroundColor(Constants.AzulEscuro)
+                        }
+                        .padding(.horizontal, 80)
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(maxWidth: 730, maxHeight: 250)
+                            .background(.white)
+                            .cornerRadius(30)
+                        
+                        HStack(alignment: .top, spacing: 20) {
+                            Text("3")
+                                .font(.system(size: 130, weight: .heavy))
+                                .foregroundColor(Constants.AzulEscuro)
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100, alignment: .center)
+                            
+                            VStack (alignment:.leading, spacing: 10) {
+                                Text("Faça seu prontuário e compartilhe-o quando estiver preenchido!")
+                                    .font(.system(size: 32, weight: .semibold))
+                                    .foregroundColor(Constants.AzulEscuro)
+                                
+                                Text("O prontuário suporta o uso de Apple Pencil e similares, permitindo anotações manuais")
+                                    .font(.system(size: 24, weight: .regular))
+                                    .foregroundColor(Constants.AzulEscuro)
+                            }
+                            
+                        }
+                        
+                        .padding(.horizontal, 80)
+                    }
+                }
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {}){
+                        Text("Próximo")
+                            .font(.system(size: 32, weight: .bold))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Constants.AzulEscuro)
+                        
+                        Image(systemName: "chevron.right")
+                            .font(.system(size: 32, weight: .bold))
+                            .foregroundColor(Constants.AzulEscuro)
+                    }
+                    .foregroundColor(.clear)
+                    .frame(width: 350, height: 60)
+                    .background(.white)
+                    .cornerRadius(18)
+                    .padding(.top, 30)
+                }
+                .padding(.horizontal, 50)
+            }
             
-            Image("VetPad")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+
+            
             
         }
         
