e6b8957d40d25853593c2dcd2a557d7a652fdd38
jasminisantos <jasminirebeca10@gmail.com>
2023-11-12 14:19:02 -0300

    adjusting vet profile and tutor profile forms

--- Mini3/View/TutorProfileView.swift
+++ Mini3/View/TutorProfileView.swift
@@ -14,18 +14,14 @@ struct TutorProfileView: View {
       static let Branco: Color = Color(red: 0.94, green: 0.95, blue: 0.95)
 
     }
-    @State private var nomeTutor = ""
-    @State private var cpf = ""
-    @State private var telefone = ""
-    @State private var endereco = ""
-    @State private var email = ""
+    
+    @ObservedObject var viewModel = PetProfileViewModel(pet: PetModel(name: "Buddy", species: "Dog", breed: "Labrador", age: "5", gender: "Male", furColor: "Yellow", tutor: "John Doe", cpf: "123.456.789-00", address: "1234 Main St"), petOwner: PetOwnerModel(name: "John Doe", cpf: "123.456.789-00", phoneNumber: "555-1234", address: "1234 Elm Street", email: "johndoe@example.com"))
     
     var body: some View {
         ZStack {
             //retângulo de fundo
             Rectangle()
                 .foregroundColor(.clear)
-                .frame(maxWidth: 730, maxHeight: 650)
                 .background(.white)
                 .overlay(RoundedRectangle(cornerRadius: 30).inset(by: 0.5).stroke(Constants.AzulClaro, lineWidth: 1))
                 .cornerRadius(30)
@@ -33,7 +29,7 @@ struct TutorProfileView: View {
             VStack {
                 
                     //DADOS DA DIREITA
-                    VStack (alignment: .leading, spacing: 8){
+                    VStack (alignment: .leading, spacing: 20){
                         HStack {
                             Text("Dados do Tutor")
                                 .font(.system(size: 32, weight: .bold))
@@ -41,114 +37,101 @@ struct TutorProfileView: View {
                             .padding(.bottom, 25)
                             
                             //BOTÃO DE EDIÇÃO
-//                            Spacer()
-//                            
-//                            Button(action: {}) {
-//                                Image(systemName: "square.and.pencil")
-//                                    .font(.system(size: 28, weight: .bold))
-//                                    .foregroundColor(Constants.Branco)
-//                                    .frame(width: 50, height: 50)
-//                                    .background(Constants.AzulEscuro)
-//                                    .cornerRadius(360)
-//                            }
+                            if viewModel.currentState == .editing {
+                                Spacer()
+                                
+                                Button(action: {}) {
+                                    Image(systemName: "square.and.pencil")
+                                        .font(.system(size: 28, weight: .bold))
+                                        .foregroundColor(Constants.Branco)
+                                        .frame(width: 50, height: 50)
+                                        .background(Constants.AzulEscuro)
+                                        .cornerRadius(360)
+                                }
+                            }
 
                         }
-                        
-                        Text("Nome do Tutor:")
-                            .font(.system(size: 24, weight: .medium))
-                            .foregroundColor(.black)
-                        
-                        TextField("", text: $nomeTutor)
-                            .font(.system(size: 24, weight: .medium))
-                            .foregroundColor(Constants.AzulEscuro)
-                            .frame(width: .infinity, height: 50)
-                            .background(Color(red: 0.88, green: 0.92, blue: 0.95))
-                            .cornerRadius(14)
-                            .overlay(RoundedRectangle(cornerRadius: 14).inset(by: 0.5).stroke(Constants.AzulClaro, lineWidth: 1))
-                        
-                        
-                        HStack (spacing: 30){
-                            VStack (alignment: .leading){
-                                Text("CPF:")
+                        VStack(alignment: .leading) {
+                            VStack(alignment: .leading) {
+                                Text("Nome do Tutor:")
                                     .font(.system(size: 24, weight: .medium))
                                     .foregroundColor(.black)
                                 
-                                
-                                TextField("", text: $cpf)
-                                    .font(.system(size: 24, weight: .medium))
-                                    .foregroundColor(Constants.AzulEscuro)
-                                    .frame(width: .infinity, height: 50)
-                                    .background(Color(red: 0.88, green: 0.92, blue: 0.95))
-                                    .cornerRadius(14)
-                                    .overlay(RoundedRectangle(cornerRadius: 14).inset(by: 0.5).stroke(Constants.AzulClaro, lineWidth: 1))
+                                CustomTextField(text: $viewModel.petOwner.name, placeholder: "", backgroundColor: Color(red: 0.88, green: 0.92, blue: 0.95), borderColor: Constants.AzulClaro, onCommit: {
+                                    
+                                })
                             }
-                            VStack (alignment: .leading){
-                                Text("Telefone:")
+                            
+                            HStack (spacing: 30){
+                                VStack (alignment: .leading){
+                                    Text("CPF:")
+                                        .font(.system(size: 24, weight: .medium))
+                                        .foregroundColor(.black)
+                                    
+                                    
+                                    CustomTextField(text: $viewModel.petOwner.cpf, placeholder: "", backgroundColor: Color(red: 0.88, green: 0.92, blue: 0.95), borderColor: Constants.AzulClaro, onCommit: {
+                                        
+                                    })
+                                }
+                                VStack (alignment: .leading){
+                                    Text("Telefone:")
+                                        .font(.system(size: 24, weight: .medium))
+                                        .foregroundColor(.black)
+                                    
+                                    
+                                    CustomTextField(text: $viewModel.petOwner.phoneNumber, placeholder: "", backgroundColor: Color(red: 0.88, green: 0.92, blue: 0.95), borderColor: Constants.AzulClaro, onCommit: {
+                                        
+                                    })
+                                }
+                            }
+                            .padding(.vertical, 15)
+                            VStack(alignment: .leading) {
+                                Text("Endereço:")
                                     .font(.system(size: 24, weight: .medium))
                                     .foregroundColor(.black)
                                 
-                                
-                                TextField("", text: $telefone)
+                                CustomTextField(text: $viewModel.petOwner.address, placeholder: "", backgroundColor: Color(red: 0.88, green: 0.92, blue: 0.95), borderColor: Constants.AzulClaro, onCommit: {
+                                    
+                                })
+                            }
+                            VStack(alignment: .leading) {
+                                Text("E-mail:")
                                     .font(.system(size: 24, weight: .medium))
-                                    .foregroundColor(Constants.AzulEscuro)
-                                    .frame(width: .infinity, height: 50)
-                                    .background(Color(red: 0.88, green: 0.92, blue: 0.95))
-                                    .cornerRadius(14)
-                                    .overlay(RoundedRectangle(cornerRadius: 14).inset(by: 0.5).stroke(Constants.AzulClaro, lineWidth: 1))
+                                    .foregroundColor(.black)
+                                
+                                CustomTextField(text: $viewModel.petOwner.email, placeholder: "", backgroundColor: Color(red: 0.88, green: 0.92, blue: 0.95), borderColor: Constants.AzulClaro, onCommit: {
+                                    
+                                })
                             }
                         }
-                        .padding(.vertical, 15)
-                        
-                        Text("Endereço:")
-                            .font(.system(size: 24, weight: .medium))
-                            .foregroundColor(.black)
-                        
-                        TextField("", text: $endereco)
-                            .font(.system(size: 24, weight: .medium))
-                            .foregroundColor(Constants.AzulEscuro)
-                            .frame(width: .infinity, height: 50)
-                            .background(Color(red: 0.88, green: 0.92, blue: 0.95))
-                            .cornerRadius(14)
-                            .overlay(RoundedRectangle(cornerRadius: 14).inset(by: 0.5).stroke(Constants.AzulClaro, lineWidth: 1))
-                        
-                        Text("E-mail:")
-                            .font(.system(size: 24, weight: .medium))
-                            .foregroundColor(.black)
-                        
-                        TextField("", text: $email)
-                            .font(.system(size: 24, weight: .medium))
-                            .foregroundColor(Constants.AzulEscuro)
-                            .frame(width: .infinity, height: 50)
-                            .background(Color(red: 0.88, green: 0.92, blue: 0.95))
-                            .cornerRadius(14)
-                            .overlay(RoundedRectangle(cornerRadius: 14).inset(by: 0.5).stroke(Constants.AzulClaro, lineWidth: 1))
                         
                     }//FIM DA VSTACK
                     
                 //BOTÃO DE SALVAR ALTERAÇÕES
-//                Button(action: {}){
-//                    Image(systemName: "checkmark")
-//                        .font(.system(size: 32, weight: .bold))
-//                        .foregroundColor(Constants.Branco)
-//
-//                    Text("Salvar alterações")
-//                        .font(.system(size: 32, weight: .bold))
-//                        .multilineTextAlignment(.center)
-//                        .foregroundColor(Constants.Branco)
-//                }
-//                .foregroundColor(.clear)
-//                .frame(width: 350, height: 60)
-//                .background(Constants.AzulClaro)
-//                .cornerRadius(18)
-//                .padding(.top, 30)
-                
+                if viewModel.currentState == .editing {
+                    Button(action: {}){
+                        Image(systemName: "checkmark")
+                            .font(.system(size: 32, weight: .bold))
+                            .foregroundColor(Constants.Branco)
+
+                        Text("Salvar alterações")
+                            .font(.system(size: 32, weight: .bold))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Constants.Branco)
+                    }
+                    .foregroundColor(.clear)
+                    .frame(width: 350, height: 60)
+                    .background(Constants.AzulClaro)
+                    .cornerRadius(18)
+                    .padding(.top, 30)
+                }
                 Image(systemName: "person.fill")
                     .font(.system(size: 32, weight: .bold))
                     .foregroundColor(Constants.AzulClaro)
-                    .padding(.top, 30)
+                    .padding(.vertical, 30)
                 
             }//FIM DA VSTACK
-            .padding(100)
+            .padding(40)
             
         }//FIM DA ZSTACK
         .background(.clear)
--- Mini3/View/VetProfileView.swift
+++ Mini3/View/VetProfileView.swift
@@ -6,104 +6,206 @@
 //
 
 import SwiftUI
+import CloudKit
 
 struct VetProfileView: View {
-    @State var text: String = ""
+    @ObservedObject var viewModel: VetProfileViewModel
+    @ObservedObject private var notePadViewModel = NotePadViewModel(lineType: .none)
+    
+    var header: some View {
+        Header(title: CustomLabels.createProfile.rawValue, backgroundColor: CustomColor.customDarkBlue, textColor: .white, arrowColor: CustomColor.customOrange)
+        
+    }
     
     var body: some View {
-        VStack{
-            Text("Criar seu perfil")
-                .fontWeight(.semibold)
-                .font(.largeTitle)
-                .padding(.vertical)
-            VStack{
-                
-                VStack (alignment: .leading) {
-                    Text("Dados do Médico Veterinário")
-                        .fontWeight(.semibold)
-                        .font(.title)
-                        .padding(.vertical)
-                    
-                    Text("Nome completo:")
-                        .font(.title2)
-                    
-                    CustomTextField(text: $text, placeholder: "", onCommit: {
-                        
-                    })
-                    .padding(.bottom)
-                    HStack{
-                        VStack(alignment: .leading){
-                            Text("CRMV:")
-                                .font(.title2)
-                            CustomTextField(text: $text, placeholder: "", onCommit: {
-                                
-                            })
-                        }
-                        
-                        .padding(.trailing)
-                        VStack(alignment: .leading) {
-                            Text("Telefone:")
-                                .font(.title2)
-                            CustomTextField(text: $text, placeholder: "", onCommit: {
-                                
-                            })
-                            
-                        }
-                    }
-                    .padding(.bottom)
-                    
-                    
-                    Text("E-mail:")
-                        .font(.title2)
-                    
-                    CustomTextField(text: $text, placeholder: "", onCommit: {
-                        
-                    })
-                    .padding(.bottom)
-                    
-                    Text("CPF")
-                        .font(.title2)
-                    
-                    CustomTextField(text: $text, placeholder: "", onCommit: {
-                        
-                    })
-                    .padding(.bottom)
-                    
-                    Text("Sua assinatura:")
-                        .font(.title2)
-                    CustomTextField(text: $text, placeholder: "", onCommit: {
-                        
-                    }) // trocar pelo espaço de desenho
-                    
-                    .padding(.bottom)
-                    
-                    HStack{
-                        Spacer()
-                        Image(systemName: "stethoscope")
-                            .font(.title)
-                            .foregroundColor(Color("Menta"))
-                        Spacer()
-                        
-                    }
-                    .padding(.vertical)
+        ScrollView {
+            VStack {
+                NavigationLink(destination: MenuView(viewModel: viewModel), isActive: $viewModel.navigateToMenuView) {
+                    EmptyView()
                 }
-                .padding(30)
-                
-                
+                header
+                VStack {
+                    form
+                    checkButton
+                    formButton
+                }
+                .frame(maxHeight: .infinity)
+                .padding(.horizontal, 70)
             }
-            .background(
-                Color("Menta")
-                    .opacity(0.2)
-                    .cornerRadius(29)
-            )
         }
-        .padding(70)
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            notePadViewModel.initializeCanvasDrawings(count: 1)
+            viewModel.checkiCloudStatus()
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+            viewModel.checkiCloudStatus()
+        }
+        .alert(isPresented: $viewModel.showiCloudAlert) {
+            alert
+        }
+    }
+    
+    var form: some View {
+        VStack{
+            VStack (alignment: .leading) {
+                Text("Dados do Médico Veterinário")
+                    .fontWeight(.semibold)
+                    .font(.title)
+                    .padding(.vertical)
+                
+                Text("Nome completo:")
+                    .font(.system(size: 24, weight: .medium))
+                
+                CustomTextField(text: $viewModel.name, placeholder: "", backgroundColor: .white, borderColor: CustomColor.customPaletteYellow, onCommit: {
+                    
+                })
+                .padding(.bottom)
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("CRMV:")
+                            .font(.system(size: 24, weight: .medium))
+                        CustomTextField(text: $viewModel.crmv, placeholder: "", backgroundColor: .white, borderColor: CustomColor.customPaletteYellow, onCommit: {
+                            
+                        })
+                    }
+                    
+                    .padding(.trailing)
+                    VStack(alignment: .leading) {
+                        Text("Telefone:")
+                            .font(.system(size: 24, weight: .medium))
+                        CustomTextField(text: $viewModel.phone, placeholder: "", backgroundColor: .white, borderColor: CustomColor.customPaletteYellow, onCommit: {
+                            
+                        })
+                        
+                    }
+                }
+                .padding(.bottom)
+                
+                
+                Text("E-mail:")
+                    .font(.system(size: 24, weight: .medium))
+                
+                CustomTextField(text: $viewModel.email, placeholder: "", backgroundColor: .white, borderColor: CustomColor.customPaletteYellow, onCommit: {
+                    
+                })
+                .padding(.bottom)
+                
+                Text("CPF")
+                    .font(.system(size: 24, weight: .medium))
+                
+                CustomTextField(text: $viewModel.cpf, placeholder: "", backgroundColor: .white, borderColor: CustomColor.customPaletteYellow, onCommit: {
+                    
+                })
+                .padding(.bottom)
+                
+                Text("Sua assinatura:")
+                    .font(.system(size: 24, weight: .medium))
+                
+                ZStack {
+                    NotePad(viewModel: notePadViewModel)
+                        .cornerRadius(20)
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(CustomColor.customPaletteYellow, lineWidth: 1)
+                        .padding(2)
+                }
+                .frame(width: .infinity, height: 100)
+                .padding(.bottom)
+                    
+                HStack {
+                    UndoRedoToolbar(orientation: .horizontal, undoAction: {
+                        notePadViewModel.undo()
+                    }, redoAction: {
+                        notePadViewModel.redo()
+                    })
+                    HStack {
+                        Spacer()
+                        Text("Esta assinatura será utilizada nos documentos de prontuários")
+                            .font(.system(size: 24, weight: .light))
+                        Spacer()
+                    }
+                }
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "stethoscope")
+                        .font(.title)
+                        .foregroundColor(Color("Amarelo"))
+                    Spacer()
+                    
+                }
+                .padding(.vertical)
+            }
+            .padding(30)
+            
+            
+        }
+        .background(
+            Color(CustomColor.customLightOrange)
+                .cornerRadius(29)
+        )
+        .padding(.top, 30)
+        
+        
+    }
+    
+    var formButton: some View {
         HStack{
             Spacer()
-//            CustomButton(title: "Próximo", action: {}, backgroundColor: Color("Menta"), textColor: Color("Azul"))
+            CustomButton(title: "Próximo", backgroundColor: Color("Menta"), textColor: .black, rightIcon: "chevron.right", width: 200, action: {
+                viewModel.checkiCloudStatus()
+                if !viewModel.showiCloudAlert && !viewModel.accountExists && viewModel.isChecked{
+                    notePadViewModel.saveDrawing()
+                    notePadViewModel.getImages()
+                    if let image = notePadViewModel.canvasImages.first {
+                        viewModel.signatureImage = image
+                    }
+                    viewModel.createDoctorData()
+                }
+            })
         }
-        .padding(.horizontal, 70)
+        .padding(.top, 30)
     }
+    
+    var checkButton: some View {
+        HStack {
+            CheckButton(isChecked: $viewModel.isChecked, borderColor: CustomColor.customPaletteYellow)
+
+            Text("Declaro que li e concordo com os ")
+                .font(.system(size: 24, weight: .regular))
+
+            NavigationLink(destination: TermsAndConditionsView()) {
+                Text("Termos e Condições Gerais de Uso")
+                    .underline()
+                    .foregroundColor(.blue)
+            }
+            .font(.system(size: 24, weight: .regular))
+        }
+        .padding(.top)
+    }
+    
+    var alert: Alert {
+        guard let alertType = viewModel.activeAlert else {
+            return Alert(title: Text("Error"), message: Text("Unknown error."))
+        }
+
+        switch alertType {
+        case .iCloudError:
+            return Alert(
+                title: Text("iCloud Account Required"),
+                message: Text("Please sign in to your iCloud account to enable all features of this app."),
+                primaryButton: .default(Text("Open Settings"), action: viewModel.openSettings),
+                secondaryButton: .cancel()
+            )
+        case .validationError(let message):
+            return Alert(title: Text("Validation Error"), message: Text(message))
+        case .customError(let message):
+            return Alert(title: Text("Error"), message: Text(message))
+        }
+    }
+
+
 }
 
 #Preview {
