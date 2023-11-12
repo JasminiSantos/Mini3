3d0e5138d90eecee4412c4d5f590c7abe70c6248
jasminisantos <jasminirebeca10@gmail.com>
2023-11-12 14:18:17 -0300

    creating terms and conditions view

--- Mini3/View/TermsAndConditionsView.swift
+++ Mini3/View/TermsAndConditionsView.swift
@@ -5,4 +5,16 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 10/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct TermsAndConditionsView: View {
+    @State var errorMessage: String? = ""
+    
+    var body: some View {
+        VStack {
+            WebView(url: URL(string: "https://vetpad.notion.site/vetpad/Termos-e-Condi-es-Gerais-de-Uso-do-aplicativo-VETPAD-53da2232b4a2441f8b3ce4b68e5834e2")!, errorMessage: $errorMessage)
+                .navigationBarTitle("Termos e Condições", displayMode: .inline)
+        }
+    }
+}
+
