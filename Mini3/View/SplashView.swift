c786a648965012798dc3fea3cbdcca5b862508ca
jasminisantos <jasminirebeca10@gmail.com>
2023-11-12 14:10:40 -0300

    creating splash view

--- Mini3/View/SplashView.swift
+++ Mini3/View/SplashView.swift
@@ -5,4 +5,19 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 08/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct SplashView: View {
+
+    var body: some View {
+        VStack {
+            Image("VetPad")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(CustomColor.customDarkBlue)
+    }
+}
