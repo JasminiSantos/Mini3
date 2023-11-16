5d7477d0d11b86975a40e7828447257aaefaea66
jasminisantos <jasminirebeca10@gmail.com>
2023-11-16 01:46:37 -0300

    creating appointment pdf view

--- Mini3/View/AppointmentPDFView.swift
+++ Mini3/View/AppointmentPDFView.swift
@@ -5,4 +5,29 @@
 //  Created by Jasmini Rebecca Gomes dos Santos on 14/11/23.
 //
 
-import Foundation
+import SwiftUI
+import PDFKit
+
+struct AppointmentPDFView: View {
+    var pdfURL: String?
+    
+    var header: some View {
+        Header(title: CustomLabels.appointment.rawValue, backgroundColor: CustomColor.customDarkBlue, textColor: .white, arrowColor: CustomColor.customOrange)
+        
+    }
+    var pdf: some View {
+        VStack {
+            if let pdfURL, let url = URL(string: pdfURL) {
+                PDFReader(url: url)
+                    .edgesIgnoringSafeArea(.all)
+            } else {
+                Text("PDF not found")
+            }
+        }
+    }
+
+    var body: some View {
+        pdf
+    }
+}
+
