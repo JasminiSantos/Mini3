8290f4a4296917fa10e57daee9cd759c1ed0c0ba
jasminisantos <jasminirebeca10@gmail.com>
2023-11-16 01:42:47 -0300

    adjusting menu navigation

--- Mini3/ContentView.swift
+++ Mini3/ContentView.swift
@@ -13,23 +13,33 @@ struct ContentView: View {
 
     @ViewBuilder
     private var currentView: some View {
-        if viewModel.viewState == .loading  {
+        switch viewModel.viewState {
+        case .loading:
             SplashView()
-        } else if viewModel.viewState == .menu {
+        case .menu:
             MenuView(viewModel: MenuViewModel(veterinarian: viewModel.veterinarian!))
-        } else if viewModel.viewState == .vetProfile || viewModel.viewState == .iCloudError {
+        case .vetProfile, .iCloudError:
+            VetProfileView(viewModel: viewModel)
+        case .conditions:
             VetProfileView(viewModel: viewModel)
         }
     }
 
     var body: some View {
         NavigationStack {
-            currentView
+            ZStack {
+                currentView
+
+                if viewModel.showOnboarding {
+                    OnboardingView(showOnboarding: $viewModel.showOnboarding)
+                }
+            }
         }
         .accentColor(CustomColor.customOrange)
         .onAppear {
             viewModel.viewState = .loading
             DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+                viewModel.showOnboarding = UserDefaults.standard.bool(forKey: "hasCompletedOnboarding")
                 if viewModel.viewState == .loading {
                     viewModel.checkiCloudStatus()
                 }
@@ -38,25 +48,25 @@ struct ContentView: View {
         .alert(isPresented: $viewModel.showiCloudAlert) {
             alert
         }
-        
-        var alert: Alert {
-            guard let alertType = viewModel.activeAlert else {
-                return Alert(title: Text("Error"), message: Text("Unknown error."))
-            }
+    }
 
-            switch alertType {
-            case .iCloudError:
-                return Alert(
-                    title: Text("iCloud Account Required"),
-                    message: Text("Please sign in to your iCloud account to enable all features of this app."),
-                    primaryButton: .default(Text("Open Settings"), action: viewModel.openSettings),
-                    secondaryButton: .cancel()
-                )
-            case .validationError(let message):
-                return Alert(title: Text("Validation Error"), message: Text(message))
-            case .customError(let message):
-                return Alert(title: Text("Error"), message: Text(message))
-            }
+    var alert: Alert {
+        guard let alertType = viewModel.activeAlert else {
+            return Alert(title: Text("Error"), message: Text("Unknown error."))
+        }
+
+        switch alertType {
+        case .iCloudError:
+            return Alert(
+                title: Text("iCloud Account Required"),
+                message: Text("Please sign in to your iCloud account to enable all features of this app."),
+                primaryButton: .default(Text("Open Settings"), action: viewModel.openSettings),
+                secondaryButton: .cancel()
+            )
+        case .validationError(let message):
+            return Alert(title: Text("Validation Error"), message: Text(message))
+        case .customError(let message):
+            return Alert(title: Text("Error"), message: Text(message))
         }
     }
 }
